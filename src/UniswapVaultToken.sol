4d2711ca7c3307ba84e019923f26c160dd4938ae
Ratimon <ratixoxo@gmail.com>
2023-06-29 14:39:08 +0700

    Fix & Feat (./src): Making interface and contract supports 2 asset tokens

--- src/UniswapVaultToken.sol
+++ src/UniswapVaultToken.sol
@@ -9,6 +9,7 @@ import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IER
 import {IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
 
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
+import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
@@ -18,33 +19,53 @@ import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.s
 
 abstract contract UniswapVaultToken is IUniswapVaultToken, ERC20, Initializable  {
     using Math for uint256;
+    using SafeMath for uint256;
 
-    IERC20 private _asset0;
-    IERC20 private _asset1;
+    IERC20 private _token0;
+    IERC20 private _token1;
 
-    // uint8 private immutable _underlyingDecimals0;
-    // uint8 private immutable _underlyingDecimals1;
     uint8 private _underlyingDecimals;
+
+    address public factory;
+
+    uint112 private reserve0;           // uses single storage slot, accessible via getReserves
+    uint112 private reserve1;           // uses single storage slot, accessible via getReserves
 
 
     constructor() ERC20('Uniswap V2', 'UNI-V2') {
-
+        factory = msg.sender;
     }
 
-    function __UniswapVaultToken_init(IERC20 asset0_, IERC20 asset1_) internal onlyInitializing {
-        (bool success0, uint8 asset0Decimals) = _tryGetAssetDecimals(asset0_);
+    function __UniswapVaultToken_init(IERC20 token0_, IERC20 token1_) internal initializer() {
+        (bool success0, uint8 asset0Decimals) = _tryGetAssetDecimals(token0_);
         uint8 underlyingDecimals0 = success0 ? asset0Decimals : 18;
-        _asset0 = asset0_;
+        _token0 = token0_;
 
-        (bool success1, uint8 asset1Decimals) = _tryGetAssetDecimals(asset0_);
-        // _underlyingDecimals1 = success1 ? asset1Decimals : 18;
+        (bool success1, uint8 asset1Decimals) = _tryGetAssetDecimals(token1_);
         uint8 underlyingDecimals1 = success1 ? asset1Decimals : 18;
-        _asset1 = asset1_;
+        _token1 = token1_;
 
         require(underlyingDecimals0 == underlyingDecimals1, "decimals must equal");
         _underlyingDecimals = underlyingDecimals0;
 
     }
+
+        // // update reserves and, on the first call per block, price accumulators
+        // function _update(uint balance0, uint balance1, uint112 _reserve0, uint112 _reserve1) private {
+        //     require(balance0 <= uint112(-1) && balance1 <= uint112(-1), 'UniswapV2: OVERFLOW');
+        //     uint32 blockTimestamp = uint32(block.timestamp % 2**32);
+        //     uint32 timeElapsed = blockTimestamp - blockTimestampLast; // overflow is desired
+        //     if (timeElapsed > 0 && _reserve0 != 0 && _reserve1 != 0) {
+        //         // * never overflows, and + overflow is desired
+        //         price0CumulativeLast += uint(UQ112x112.encode(_reserve1).uqdiv(_reserve0)) * timeElapsed;
+        //         price1CumulativeLast += uint(UQ112x112.encode(_reserve0).uqdiv(_reserve1)) * timeElapsed;
+        //     }
+        //     reserve0 = uint112(balance0);
+        //     reserve1 = uint112(balance1);
+        //     blockTimestampLast = blockTimestamp;
+        //     emit Sync(reserve0, reserve1);
+        // }
+    
 
     /**
      * @dev Attempts to fetch the asset decimals. A return value of false indicates that the attempt failed in some way.
@@ -63,77 +84,65 @@ abstract contract UniswapVaultToken is IUniswapVaultToken, ERC20, Initializable 
     }
 
 
-    //  function decimals0() public view virtual override(IERC20Metadata, ERC20) returns (uint8) {
-    //     return _underlyingDecimals0 + _decimalsOffset();
-    // }
-
-    // function decimals1() public view virtual override(IERC20Metadata, ERC20) returns (uint8) {
-    //     return _underlyingDecimals0 + _decimalsOffset();
-    // }
-
      function decimals() public view virtual override(IERC20Metadata, ERC20) returns (uint8) {
         return _underlyingDecimals + _decimalsOffset();
     }
 
     function asset0() public view virtual override returns (address) {
-        return address(_asset0);
+        return address(_token0);
     }
 
     function asset1() public view virtual override returns (address) {
-        return address(_asset1);
+        return address(_token1);
     }
 
-    function totalAssets() public view virtual returns (uint256);
-    
-
-    // function totalAssets() public view virtual override returns (uint256) {
-    //     (uint112 _reserve0, uint112 _reserve1,) = getReserves();
-    //     // return _asset.balanceOf(address(this));
-    //     return Math.min(a, b);
-    // }
+    function totalAssets() public view virtual returns (uint256 totalManagedAssets0, uint256 totalManagedAssets1 ) {
+        // if (totalSupply() == 0) return (0,0);
+        return (reserve0, reserve1);
+    }
     
 
     function _decimalsOffset() internal view virtual returns (uint8) {
         return 0;
     }
 
-    function convertToShares(uint256 assets) public view virtual override returns (uint256) {
-        return _convertToShares(assets, Math.Rounding.Down);
+    function convertToShares(uint256 assets0, uint256 assets1) public view virtual override returns (uint256) {
+        return _convertToShares(assets0, assets1, Math.Rounding.Down);
     }
 
-    function convertToAssets(uint256 shares) public view virtual override returns (uint256) {
+    function convertToAssets(uint256 shares) public view virtual override returns (uint256 assets0, uint256 assets1) {
         return _convertToAssets(shares, Math.Rounding.Down);
     }
 
-    function maxDeposit(address) public view virtual override returns (uint256) {
-        return type(uint256).max;
+    function maxDeposit(address) public view virtual override returns (uint256, uint256) {
+        return (type(uint256).max, type(uint256).max);
     }
 
-    function maxMint(address) public view virtual override returns (uint256) {
-        return type(uint256).max;
-    }
+    // function maxMint(address) public view virtual override returns (uint256) {
+    //     return type(uint256).max;
+    // }
 
-    function maxWithdraw(address owner) public view virtual override returns (uint256) {
-        return _convertToAssets(balanceOf(owner), Math.Rounding.Down);
-    }
+    // function maxWithdraw(address owner) public view virtual override returns (uint256, uint256) {
+    //     return _convertToAssets(balanceOf(owner), Math.Rounding.Down);
+    // }
 
     function maxRedeem(address owner) public view virtual override returns (uint256) {
         return balanceOf(owner);
     }
 
-    function previewDeposit(uint256 assets) public view virtual override returns (uint256) {
-        return _convertToShares(assets, Math.Rounding.Down);
+    function previewDeposit(uint256 assets0, uint256 assets1) public view virtual override returns (uint256) {
+        return _convertToShares(assets0,assets1, Math.Rounding.Down);
     }
 
-    function previewMint(uint256 shares) public view virtual override returns (uint256) {
-        return _convertToAssets(shares, Math.Rounding.Up);
-    }
+    // function previewMint(uint256 shares) public view virtual override returns (uint256, uint256) {
+    //     return _convertToAssets(shares, Math.Rounding.Up);
+    // }
 
-    function previewWithdraw(uint256 assets) public view virtual override returns (uint256) {
-        return _convertToShares(assets, Math.Rounding.Up);
-    }
+    // function previewWithdraw(uint256 assets0, uint256 assets1) public view virtual override returns (uint256) {
+    //     return _convertToShares(assets0, assets1, Math.Rounding.Up);
+    // }
 
-    function previewRedeem(uint256 shares) public view virtual override returns (uint256) {
+    function previewRedeem(uint256 shares) public view virtual override returns (uint256, uint256) {
         return _convertToAssets(shares, Math.Rounding.Down);
     }
 
@@ -146,7 +155,35 @@ abstract contract UniswapVaultToken is IUniswapVaultToken, ERC20, Initializable 
     //     return shares;
     // }
 
-    function deposit(uint256 assets, address receiver) public virtual override returns (uint256);
+    function deposit(uint256 assets0, uint256 assets1, address receiver) public virtual override returns (uint256 shares) {
+
+        (uint256 maxAssets0, uint256 maxAssets1) = maxDeposit(receiver);
+        require ( (assets0 <= maxAssets0) && (assets1 <= maxAssets1) , "ERC4626: deposit more than max"  );
+
+        // Need to transfer before minting to avoid reenter.
+        SafeERC20.safeTransferFrom(_token0, msg.sender, address(this), assets0);
+        SafeERC20.safeTransferFrom(_token1, msg.sender, address(this), assets1);
+
+        (uint256 _reserve0, uint256 _reserve1 )= totalAssets();
+
+        uint256 balance0 = _token0.balanceOf(address(this));
+        uint256 balance1 = _token1.balanceOf(address(this));
+
+        uint256 amount0 = balance0.sub(_reserve0);
+        uint256 amount1 = balance1.sub(_reserve1);
+
+
+        if (totalSupply() == 0) {
+            shares = Math.sqrt(amount0 * amount1);
+        } else {
+            shares  = previewDeposit(amount0, amount1) ;
+        }
+
+        _mint(receiver, shares);
+        // _update(balance0, balance1, _reserve0, _reserve1);
+
+        emit Deposit(msg.sender, receiver, assets0, assets1, shares);
+    }
 
 
     // function mint(uint256 shares, address receiver) public virtual override returns (uint256) {
@@ -158,7 +195,7 @@ abstract contract UniswapVaultToken is IUniswapVaultToken, ERC20, Initializable 
     //     return assets;
     // }
 
-    function mint(uint256 shares, address receiver) public virtual override returns (uint256);
+    // function mint(uint256 shares, address receiver) public virtual override returns (uint256, uint256);
 
     // function withdraw(uint256 assets, address receiver, address owner) public virtual override returns (uint256) {
     //     require(assets <= maxWithdraw(owner), "ERC4626: withdraw more than max");
@@ -169,7 +206,7 @@ abstract contract UniswapVaultToken is IUniswapVaultToken, ERC20, Initializable 
     //     return shares;
     // }
 
-    function withdraw(uint256 assets, address receiver, address owner) public virtual override returns (uint256);
+    // function withdraw(uint256 assets0, uint256 assets1, address receiver, address owner) public virtual override returns (uint256);
 
     // function redeem(uint256 shares, address receiver, address owner) public virtual override returns (uint256) {
     //     require(shares <= maxRedeem(owner), "ERC4626: redeem more than max");
@@ -180,14 +217,47 @@ abstract contract UniswapVaultToken is IUniswapVaultToken, ERC20, Initializable 
     //     return assets;
     // }
 
-    function redeem(uint256 shares, address receiver, address owner) public virtual override returns (uint256);
+    function redeem(uint256 shares, address receiver, address owner) public virtual override returns (uint256, uint256) {
+        require(shares <= maxRedeem(owner), "ERC4626: redeem more than max");
 
-    function _convertToShares(uint256 assets, Math.Rounding rounding) internal view virtual returns (uint256) {
-        return assets.mulDiv(totalSupply() + 10 ** _decimalsOffset(), totalAssets() + 1, rounding);
+        (uint256 assets0, uint256 assets1) = previewRedeem(shares);
+        require(assets0 > 0 && assets1 > 0, 'UniswapV2: INSUFFICIENT_LIQUIDITY_BURNED');
+        _burn(address(this), shares);
+
+        // Need to transfer before returning asser to avoid reenter.
+        SafeERC20.safeTransfer(_token0, receiver, assets0);
+        SafeERC20.safeTransfer(_token1, receiver, assets1);
+
+        uint256 balance0 = _token0.balanceOf(address(this));
+        uint256 balance1 = _token1.balanceOf(address(this));
+
+        emit Withdraw(msg.sender, receiver, owner, assets0,assets1, shares);
+    
+
+        return (assets0, assets1);
+
     }
 
-    function _convertToAssets(uint256 shares, Math.Rounding rounding) internal view virtual returns (uint256) {
-        return shares.mulDiv(totalAssets() + 1, totalSupply() + 10 ** _decimalsOffset(), rounding);
+    function _convertToShares(uint256 assets0, uint256 assets1, Math.Rounding rounding) internal view virtual returns (uint256) {
+
+        (uint256 _reserve0, uint256 _reserve1 )= totalAssets();
+
+        uint256 liquidity0 = assets0.mulDiv(totalSupply() + 10 ** _decimalsOffset(), _reserve0 + 1, rounding);
+        uint256 liquidity1 = assets1.mulDiv(totalSupply() + 10 ** _decimalsOffset(), _reserve1 + 1, rounding);
+
+        return Math.min(liquidity0, liquidity1);
+    }
+
+    function _convertToAssets(uint256 shares, Math.Rounding rounding) internal view virtual returns (uint256, uint256) {
+
+        // (uint256 balance0, uint256 balance1 )= totalAssets();
+        uint256 balance0 = _token0.balanceOf(address(this));
+        uint256 balance1 = _token1.balanceOf(address(this));
+
+        return (
+            shares.mulDiv(balance0 + 1, totalSupply() + 10 ** _decimalsOffset(), rounding),
+            shares.mulDiv(balance1 + 1, totalSupply() + 10 ** _decimalsOffset(), rounding)
+        );
     }
 
     // function _deposit(address caller, address receiver, uint256 assets, uint256 shares) internal virtual {
@@ -197,7 +267,7 @@ abstract contract UniswapVaultToken is IUniswapVaultToken, ERC20, Initializable 
     //     emit Deposit(caller, receiver, assets, shares);
     // }
 
-    function _deposit(address caller, address receiver, uint256 assets, uint256 shares) internal virtual;
+    // function _deposist(address caller, address receiver, uint256 assets, uint256 shares) internal virtual;
 
     // function _withdraw(
     //     address caller,
@@ -222,15 +292,13 @@ abstract contract UniswapVaultToken is IUniswapVaultToken, ERC20, Initializable 
     //     emit Withdraw(caller, receiver, owner, assets, shares);
     // }
 
-    function _withdraw(
-        address caller,
-        address receiver,
-        address owner,
-        uint256 assets,
-        uint256 shares
-    ) internal virtual;
+    // function _withdraw(
+    //     address caller,
+    //     address receiver,
+    //     address owner,
+    //     uint256 assets,
+    //     uint256 shares
+    // ) internal virtual;
     
-
-
 }
 
--- src/interfaces/IUniswapVaultToken.sol
+++ src/interfaces/IUniswapVaultToken.sol
@@ -8,48 +8,48 @@ import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IER
 
 
 interface IUniswapVaultToken is IERC20, IERC20Metadata {
-    event Deposit(address indexed sender, address indexed owner, uint256 assets, uint256 shares);
+    event Deposit(address indexed sender, address indexed owner, uint256 assets0,uint256 assets1, uint256 shares);
 
     event Withdraw(
         address indexed sender,
         address indexed receiver,
         address indexed owner,
-        uint256 assets,
+        uint256 assets0,
+        uint256 assets1,
         uint256 shares
     );
-
 
     function asset0() external view returns (address assetTokenAddress);
 
     function asset1() external view returns (address assetTokenAddress);
 
-    function totalAssets() external view returns (uint256 totalManagedAssets);
+    function totalAssets() external view returns (uint256 totalManagedAssets0, uint256 totalManagedAssets1 );
 
-    function convertToShares(uint256 assets) external view returns (uint256 shares);
+    function convertToShares(uint256 assets0, uint256 assets1) external view returns (uint256 shares);
 
-    function convertToAssets(uint256 shares) external view returns (uint256 assets);
+    function convertToAssets(uint256 shares) external view returns (uint256 assets0, uint256 assets1);
 
-    function maxDeposit(address receiver) external view returns (uint256 maxAssets);
+    function maxDeposit(address receiver) external view returns (uint256 maxAssets0, uint256 maxAssets1 );
 
-    function previewDeposit(uint256 assets) external view returns (uint256 shares);
+    function previewDeposit(uint256 assets0, uint256 assets1) external view returns (uint256 shares);
 
-    function deposit(uint256 assets, address receiver) external returns (uint256 shares);
+    function deposit(uint256 assets0, uint256 assets1, address receiver) external returns (uint256 shares);
 
-    function maxMint(address receiver) external view returns (uint256 maxShares);
+    // function maxMint(address receiver) external view returns (uint256 maxShares);
 
-    function previewMint(uint256 shares) external view returns (uint256 assets);
+    // function previewMint(uint256 shares) external view returns (uint256 assets0, uint256 assets1);
 
-    function mint(uint256 shares, address receiver) external returns (uint256 assets);
+    // function mint(uint256 shares, address receiver) external returns (uint256 assets0, uint256 assets1);
 
-    function maxWithdraw(address owner) external view returns (uint256 maxAssets);
+    // function maxWithdraw(address owner) external view returns (uint256 maxAssets0, uint256 maxAssets1 );
 
-    function previewWithdraw(uint256 assets) external view returns (uint256 shares);
+    // function previewWithdraw(uint256 assets0, uint256 assets1) external view returns (uint256 shares);
 
-    function withdraw(uint256 assets, address receiver, address owner) external returns (uint256 shares);
+    function withdraw(uint256 assets0, uint256 assets1, address receiver, address owner) external returns (uint256 shares);
 
     function maxRedeem(address owner) external view returns (uint256 maxShares);
 
-    function previewRedeem(uint256 shares) external view returns (uint256 assets);
+    function previewRedeem(uint256 shares) external view returns (uint256 assets0, uint256 assets1);
 
-    function redeem(uint256 shares, address receiver, address owner) external returns (uint256 assets);
+    function redeem(uint256 shares, address receiver, address owner) external returns (uint256 assets0, uint256 assets1);
 }
